dff99d9ce5ac741c89ec6ad1618e24f54d75f02c
Rémy Salim <salim.remy@gmail.com>
2020-06-02 23:01:46 +0200

    Add .travis.yml file and a demo gif to README.md

--- include/cppshell/Value.hpp
+++ include/cppshell/Value.hpp
@@ -3,13 +3,35 @@
 
 namespace cppshell {
 namespace values {
+
+template<typename T, typename Text>
+static constexpr T as(const Text&& text) {
+    return as<T>(text);
+}
+
+template<typename T, typename Text>
+static constexpr T as(const Text& text) {
+    if constexpr (std::is_same_v<std::decay_t<Text>, T>)
+        return text;
+    else if constexpr (std::is_same_v<bool, T>)
+        return bool_parser(text);
+    else if constexpr (std::is_integral_v<T>)
+        return integer_parser<T>(text);
+    else
+#ifdef CPPSHELL_HAS_STREAM_OPERATORS
+        return stringstream_parser<T>(text);
+#else
+        static_assert(detail::dependent_false<T>::value, "don't know how to parse");
+#endif
+}
+
 class ValueString {
 public:
     explicit ValueString(std::string& str) : text(str) {}
 
     template<typename T>
     T as() const {
-        return ::cppshell::as<T>(text);
+        return ::cppshell::values::as<T>(text);
     }
     const std::string& operator()() const { return text; }
 
